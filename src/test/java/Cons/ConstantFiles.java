259d736627a62717d64c84eaa9ae4b3a11ce8c72
Jay Zhanje <jay@Jays-MacBook-Pro.local>
2026-08-24 06:58:22 -0400

    CI Update Flow

--- src/test/java/Cons/ConstantFiles.java
+++ src/test/java/Cons/ConstantFiles.java
@@ -11,7 +11,7 @@ public class ConstantFiles {
 	public static final String HOME_TEST_DATA_FILE_PATH = ROOT_PATH + "/src/test/java/testData/hometestdata.properties";
 	public static final String TEST_FILE_UPLOAD_PATH = ROOT_PATH+"/src/test/resources/Code.docx";
 	public static final String TEST_PHOTO_UPLOAD_PATH = ROOT_PATH+"/src/test/resources/FIFA-22.jpg";
-	public static final String SCREENSHOTS_FOLDER_PATH = ROOT_PATH+"/src/test/resources/Reports/"+CommonUtilities.getTimeStamp()+"_Lidl.PNG";
-	public static final String REPORTS_FILE_PATH = ROOT_PATH+"/src/test/resources/Reports/"+CommonUtilities.getTimeStamp()+"_Lidl.html";
+	public static final String SCREENSHOTS_FOLDER_PATH = ROOT_PATH+"/src/test/resources/Reports/"+CommonUtilities.getTimeStamp()+"Target.PNG";
+	public static final String REPORTS_FILE_PATH = ROOT_PATH+"/src/test/resources/Reports/"+CommonUtilities.getTimeStamp()+"Target.html";
 	
 }
